fd57de368ada5cf1ff6786d1e6fc323fc511032c
Micah <micah.mutrux@gmail.com>
2013-08-25 01:55:49 +0000

    SALAT + TUTORIAL: importing salat ORM and updated play Todo tutorial to use mongoDB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,38 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models._
 
 object Application extends Controller {
+
+  val taskForm = Form (
+    "label" -> nonEmptyText
+  )
   
   def index = Action {
-    Ok(views.html.index("If we knew what it was we were doing, it would not be called research, would it?"))
+    Redirect(routes.Application.tasks)
+  }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: String) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
   }
   
 }
--- project/Build.scala
+++ project/Build.scala
@@ -12,15 +12,12 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     "org.mongodb" % "casbah_2.10" % "2.6.2",
-    "com.novus" % "salat-core_2.10" % "1.9.2",
-    "se.radley" %% "play-plugins-salat" % "1.2"
+    "com.novus" % "salat-core_2.10" % "1.9.2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here     
-    routesImport += "se.radley.plugin.salat.Binders._",
-    templatesImport += "org.bson.types.ObjectId",
     resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/" 
   )
 
